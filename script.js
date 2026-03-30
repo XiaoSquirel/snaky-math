51cbcf8d44ded22b9c4627eebda8a674175b7698
XiaoSquirrel <osmotic_postwar_2e@icloud.com>
2026-03-30 15:56:12 +0800

    enable speed change

--- script.js
+++ script.js
@@ -419,10 +419,10 @@ document.getElementById('btn-left').addEventListener('click',  () => handleDpad(
 document.getElementById('btn-right').addEventListener('click', () => handleDpad('right'));
 
 function increaseSpeed() {
-    //if      (gameSpeedDelay > 150) gameSpeedDelay -= 3;
-    //else if (gameSpeedDelay > 100) gameSpeedDelay -= 2;
-    //else if (gameSpeedDelay > 50)  gameSpeedDelay -= 1;
-    //else if (gameSpeedDelay > 25)  gameSpeedDelay -= 1;
+    if      (gameSpeedDelay > 150) gameSpeedDelay -= 3;
+    else if (gameSpeedDelay > 100) gameSpeedDelay -= 2;
+    else if (gameSpeedDelay > 50)  gameSpeedDelay -= 1;
+    else if (gameSpeedDelay > 25)  gameSpeedDelay -= 1;
 }
 
 // Self-collision only (wall handled in move())
